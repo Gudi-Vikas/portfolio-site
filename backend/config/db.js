d728cfb2842df824e63df72d6cb677aafeed7234
Gudi-Vikas <vikasgudii@gmail.com>
2025-08-24 14:48:29 +0530

    Removed backend and admin folders from repo

--- backend/config/db.js
+++
@@ -1,7 +0,0 @@
-import mongoose from 'mongoose'
-
-export const connectDB = ()=>{
-    mongoose.connect("mongodb+srv://vikasgudii:7013713@cluster0.kns1efd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-    .then(()=>console.log("db connected")
-    )
-}
